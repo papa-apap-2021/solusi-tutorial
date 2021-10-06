e2602a62e3d5bfd4b5b21d9dbad24534f172432c
kamilakaffah07 <kamilakaffah07@gmail.com>
2021-10-06 23:52:59 +0700

    final solution for tutorial 4 pergipergi

---
+++ pergipergi/src/main/java/apap/tutorial/pergipergi/service/TravelAgensiService.java
@@ -0,0 +1,15 @@
+package apap.tutorial.pergipergi.service;
+
+import apap.tutorial.pergipergi.model.TravelAgensiModel;
+
+import java.time.LocalTime;
+import java.util.List;
+
+public interface TravelAgensiService {
+    void addAgensi(TravelAgensiModel travelAgensi);
+    List<TravelAgensiModel> getListAgensi();
+    TravelAgensiModel getAgensiByNoAgensi(Long noAgensi);
+    TravelAgensiModel updateAgensi(TravelAgensiModel travelAgensi);
+    void deleteAgensi(TravelAgensiModel travelAgensi);
+    boolean isClosed(LocalTime waktuBuka, LocalTime waktuTutup);
+}
